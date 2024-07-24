0f4ded1a1d640b559bb295a213cf0dd939daea5f
hyder <ibrahimhydsoft@gmail.com>
2024-07-24 16:26:05 -0700

    Install the taiwindcss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import Employee from "./components/employee";
 import { useState } from 'react';
 
@@ -7,7 +7,7 @@ function App() {
   const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmployees ?
         <>
         <input type="text" onChange={(e) => {
