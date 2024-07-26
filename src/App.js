a3a43ad1426688436bfa2b3f9ed68349f4200245
hyder <ibrahimhydsoft@gmail.com>
2024-07-26 09:35:38 -0700

    Introduce forms in react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import './index.css'
 import Employee from "./components/employee";
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: "Ameera",
+        role: "Developer",
+        img: "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=400",
+      },
+      {
+        name: "Amalen",
+        role: "Secrat",
+        img: "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=400",
+      },
+      {
+        name: "Hyder",
+        Role: "CEO",
+        img: "https://media.istockphoto.com/id/1366764046/photo/the-gumbaz-at-srirangapatna-is-a-muslim-mausoleum-at-the-centre-of-a-landscaped-garden.jpg?b=1&s=612x612&w=0&k=20&c=8o_f_NaQZgn-sn6Kw6VjjeTfCgYf6MZ5MqYy__HMTag=",
+      },
+      {
+        name: 'Emily Cody',
+        role: "Acct",
+        img: "https://images.pexels.com/photos/2681751/pexels-photo-2681751.jpeg?auto=compress&cs=tinysrgb&w=400",
+      },
+      {
+        name: "Samaher",
+        role: "Acccounts",
+        img: "https://images.pexels.com/photos/19378027/pexels-photo-19378027/free-photo-of-portrait-of-brunette-woman-in-sunlight.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      },
+    ]
+  );
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ?
         <>
-        <input type="text" onChange={(e) => {
-          console.log(e.target.value);
-          setRole(e.target.value);
-        }}></input>
-          <Employee name='Hyder' role='inser' />
-          <Employee  name='Ahmed' role={role}/>
-          <Employee name='Awad'/>
+          <input type="text" onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}></input>
+          <div className='flex flex-wrap justify-center'>
+            {employees.map((employee) => {
+              console.log(uuidv4());
+              return(
+              <Employee 
+              key={uuidv4()}
+              name={employee.name} 
+              role={employee.role} 
+              img={employee.img}
+              />
+              );
+            })}
+          </div>
         </>
         :
         <p>You can not see the employee</p>
--- src/components/employee.js
+++ src/components/employee.js
@@ -1,10 +1,29 @@
+import React from "react";
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) {
     return (
-        <>
-            <h3>Employee {props.name}</h3>
-            <p>{props.role ? props.role : "No role" }</p>
-        </>
-    )
+        <div class="m-2 py-8 px-8 max-w-sm rounded-xl 
+        shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 
+        sm:space-x-6">
+        <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto 
+        h-24 rounded-full sm:mx-0 sm:shrink-0" 
+         src={props.img}/>
+        <div class="text-center space-y-2 sm:text-left">
+          <div class="space-y-0.5">
+            <p class="text-lg text-black font-semibold">
+              {props.name}
+            </p>
+            <p class="text-slate-500 font-medium">
+              {props.role}
+            </p>
+            <EditEmployee />
+          </div>
+         
+        </div>
+      </div>
+      
+    );
 }
 
 export default Employee;
